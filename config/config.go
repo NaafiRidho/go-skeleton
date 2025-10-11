d2d828d833c5ee2364e9344b1aaa91a0d45decda
NaafiRidho <naafiridho0505@gmail.com>
2025-10-11 15:28:49 +0700

    feat: add configuration initialization with JSON and Consul fallback

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"os"
+	"user-service/common/utils"
+	"github.com/sirupsen/logrus"
+)
+
+var Config AppConfig
+
+type AppConfig struct {
+	Port                  int      `json:"port"`
+	AppName               string   `json:"appName"`
+	AppKey                string   `json:"appKey"`
+	SigningKey            string   `json:"signingKey"`
+	Database              Database `json:"database"`
+	RateLimiterMaxRequest float64  `json:"rateLimiterMaxRequest"`
+	RateLimiterTimeSecond int      `json:"rateLimiterTimeSecond"`
+	JwtSectetKey          string   `json:"jwtSecret"`
+	JwtExpireTime         int      `json:"jwtExpireTime"`
+}
+type Database struct {
+	Host                  string `json:"host"`
+	Port                  int    `json:"port"`
+	Name                  string `json:"name"`
+	UserName              string `json:"username"`
+	Password              string `json:"password"`
+	MaxOpenConnections    int    `json:"maxOpenConnections"`
+	MaxLifeTimeConnection int    `json:"maxLifeTimeConnection"`
+	MaxIdleConnections    int    `json:"maxIdleConnections"`
+	MaxIdleTime           int    `json:"maxIdleTime"`
+}
+
+func Init() {
+	err := utils.BindFromJson(&Config, "config.json", ".")
+	if err != nil {
+		logrus.Infof("Failed to bind config from file: %v", err)
+		err = utils.BindFromConsul(&Config, os.Getenv(os.Getenv("CONSUL_HTTP_URL")), os.Getenv("CONSUL_HTTP_KEY"))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
